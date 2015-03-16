77410938a37996020bd94df55feea864e33e1d4d
Thang Thin <thangthin@gmail.com>
2015-03-16 18:56:14 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var city = {latitude: 42.3601, longitude: -71.0589};
+// Google map options
+var mapProp = {
+    center: new google.maps.LatLng(city.latitude,city.longitude), 
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP 
+  }; 
+// Initialize map with default city marker
+function initialize() {
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  var marker = new google.maps.Marker({
+    position: mapProp.center,
+    // animates marker to draw user attention
+    // TODO: hook animation to selected list item
+    // animation:google.maps.Animation.BOUNCE
+    });
+
+  marker.setMap(map);
+
+  // sets marker to listen for click
+  // opens InfoWindow when clicked on
+  var infowindow = new google.maps.InfoWindow({
+    content: "hello"
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+//End of initialize
+}
+
+// Load map on page load
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
