cfc0dcd53b6da651727cf69bc31b31d7b0457d24
Thang Thin <thangthin@gmail.com>
2015-03-18 22:59:08 -0400

    have markers display for initial places

--- js/app.js
+++ js/app.js
@@ -1,50 +1,69 @@
-var city = {latitude: 42.3601, longitude: -71.0589};
-// Google map options
-var mapProp = {
-    center: new google.maps.LatLng(city.latitude,city.longitude), 
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP 
-  }; 
+//Starts of appviewmodel
 
-// Initialize map with default city marker
-function initialize() {
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+function AppViewModel() {
+    var self = this;
+    // AppViewModel
+    var initial_places = [
+        {name: "Fenway Park",address: "4 Yawkey Way, Boston, MA 02215",type: "landmark"}, 
+        {name: "Boston Public Garden",address: "69 Beacon St., Boston, MA 02108",type: "landmark"}, 
+        {name: "Boston Public Library",address: "700 Boylston St., At Copley Sq., Boston, MA 02116",type: "landmark"},
+        {},
+        {}
+    ];
 
-  var marker = new google.maps.Marker({
-    position: mapProp.center,
-    // animates marker to draw user attention
-    // TODO: hook animation to selected list item
-    // animation:google.maps.Animation.BOUNCE
+    this.places = ko.observable([]);
+    
+    initial_places.forEach(function(place) {
+        self.places().push(place);
     });
+    
+    // Sets google map city and properties
+    this.city = {latitude: 42.3601,longitude: -71.0589};
+    
+    // Google map options
+    this.mapProp = {
+        center: new google.maps.LatLng(self.city.latitude, self.city.longitude),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-  marker.setMap(map);
+    // Initialize map with default city marker
+    function initialize() {
+        self.map = new google.maps.Map(document.getElementById("googleMap"), self.mapProp);
+    }
 
-  // sets marker to listen for click
-  // opens InfoWindow when clicked on
-  var infowindow = new google.maps.InfoWindow({
-    content: "hello"
-  });
+    // Load map on page load
+    google.maps.event.addDomListener(window, 'load', initialize);
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+    //makes marker for every place in places
+    self.places().forEach(function(place){
+      $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+place.address+"&region=us",{},function(data){
+        //parse for place of interest latitude and longitude to set up google map marker
+        var dataLat = data.results[0].geometry.location.lat;
+        var dataLng = data.results[0].geometry.location.lng;
+        var googleMapsLatLng = new google.maps.LatLng(dataLat,dataLng);
+        var marker = new google.maps.Marker({
+          position: googleMapsLatLng
+          // animates marker to draw user attention
+          // TODO: hook animation to selected list item
+          // animation:google.maps.Animation.BOUNCE
+        });
+        marker.setMap(self.map);
+        // sets marker to listen for click
+        // opens InfoWindow when clicked on
+        var infowindow = new google.maps.InfoWindow({
+          content: "hello"
+        });
+        // add evet listener for click
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(self.map, marker);
+        });
 
-//End of initialize
-}
+      });
+      
+    })
 
-// Load map on page load
-google.maps.event.addDomListener(window, 'load', initialize);
-
-function AppViewModel(){
-  this.places = ko.observable(
-    ["test","vancouver","seattle",
-      "portland","montreal","toronto",
-      "san francisco"
-    ]);
-
+//End of AppViewModel
 }
 
 ko.applyBindings(new AppViewModel);
-
-
-
